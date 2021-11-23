08adc7b057aede1ff3bc86d694dfb6832b099e06
olekk <teletubis28@gmail.com>
2021-11-23 03:33:51 +0100

    optimization

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -6,17 +6,19 @@ import Keys from "../Keys.json";
 
 function Keyboard (props) {
 
+    // state variables with info about key and mouse events
     const [hoveredKeyId, setHoveredKeyId] = useState(null);
     const [mousedown, setMousedown] = useState(false);
     
+    // variable containing capslock boolean
     const capslock = useRef(false);
+
     // constant with Keys data without division into rows
     const KeysList = [].concat(...Keys);
 
-
     // logic combining mouse and key events which results in Keyboard output to App or toggling capslock
     let timeoutId = useRef([0,0]);
-    const handleKeyEvent = () => {
+    useEffect(() => {
 
         clearTimeout(timeoutId.current[0]);
 
@@ -46,9 +48,7 @@ function Keyboard (props) {
             clearTimeout(timeoutId.current[0]);
             clearTimeout(timeoutId.current[1]);
         }
-    }
-
-    useEffect(handleKeyEvent,[hoveredKeyId, mousedown, KeysList, props])
+    },[hoveredKeyId, mousedown, KeysList, props]);
 
     // Keyboard element is listening to mouse clicks and each key listens to its hover
     return (
@@ -60,18 +60,14 @@ function Keyboard (props) {
             {Keys.map((row, index) => 
                 <div className="keyboard__row" key={index}>
 
-                    {row.map(key => {
-                        return (
-                            <Key 
-                                key={key.id}
-                                id={key.id}
-                                value={capslock.current ? key.upper : key.lower}
-                                pressed={mousedown && hoveredKeyId === key.id}
-                                handleKeyHover={(entered)=>setHoveredKeyId(entered ? key.id : null )}
-                            />
-                        )
-                    }
-                        
+                    {row.map(key => 
+                        <Key 
+                            key={key.id}
+                            id={key.id}
+                            value={capslock.current ? key.upper : key.lower}
+                            pressed={mousedown && hoveredKeyId === key.id}
+                            handleKeyHover={(entered)=>setHoveredKeyId(entered ? key.id : null )}
+                        />
                     )}
 
                 </div>
